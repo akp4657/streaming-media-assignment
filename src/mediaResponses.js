b06dcc02b42ec32612cdb64e87d9101adbc18437
Anthony Pichardo <anthonykpichardo@gmail.com>
2020-01-28 17:24:56 -0500

    Finished Assignment

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -4,4 +4,14 @@ const getParty = (request, response) => {
   media.loadFile(request, response, '../client/party.mp4', 'video/mp4');
 };
 
+const getBird = (request, response) => {
+  media.loadFile(request, response, '../client/bird.mp4', 'video/mp4');
+};
+
+const getBling = (request, response) => {
+  media.loadFile(request, response, '../client/bling.mp3', 'audio/mpeg');
+};
+
 module.exports.getParty = getParty;
+module.exports.getBird = getBird;
+module.exports.getBling = getBling;
