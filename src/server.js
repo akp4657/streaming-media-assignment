7dc1376843128f50dc7c35400e8521fdab8f783a
Anthony Pichardo <anthonykpichardo@gmail.com>
2020-01-31 10:03:32 -0500

    Changed port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const htmlHandler = require('./htmlResponses.js');
 const mediaHandler = require('./mediaResponses.js');
 
-const port = process.env.PORT || process.env.NODE_PART || 3010;
+const port = process.env.PORT || process.env.NODE_PART || 3000;
 
 const onRequest = (request, response) => {
   console.log(request.url);
